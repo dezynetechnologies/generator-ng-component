bb94faa7e698428a65851e32700b1757d17a5970
Dheeraj Kaushik <dheeraj@dezyne.in>
2015-06-03 22:02:01 +0530

    Started modifying route generator templates

--- templates/route/name(uirouter).js
+++ templates/route/name(uirouter).js
@@ -8,4 +8,12 @@ angular.module('<%= scriptAppName %>')
         templateUrl: '<%= htmlUrl %>',
         controller: '<%= classedName %>Ctrl'
       });
-  });
+  })
+  .run( Run );
+
+  /* @inject */
+  function Run(Menus) {
+    // Set top bar menu items
+    Menus.set('<%= name %>', '<%= route %>', 'grey-500');
+  }
+
